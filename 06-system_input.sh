752824fb71fe84439eef88acad423c3d382cd5de
veridiux <veridiux@gmail.com>
2025-07-19 10:10:06 -0500

    ass

--- 06-system_input.sh
+++ 06-system_input.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
 # Ask and save configuration to config.sh
+
 # --- Hostname ---
-read -rp "❓ What is your hostname? [archlinux]: " HOSTNAME
-HOSTNAME=${HOSTNAME:-Archlinux}
+DEFAULT_HOSTNAME="Archlinux"
+read -rp "❓ What is your hostname? [$DEFAULT_HOSTNAME]: " HOSTNAME
+HOSTNAME=${HOSTNAME:-$DEFAULT_HOSTNAME}
 echo "$HOSTNAME" > /etc/hostname
 echo "HOSTNAME=\"$HOSTNAME\"" >> config.sh
 
 # --- Timezone ---
-read -rp "🌍 What is your timezone? [UTC] (e.g., Europe/London): " TIMEZONE
-TIMEZONE=${TIMEZONE:-America/Chicago}
+DEFAULT_TIMEZONE="America/Chicago"
+read -rp "🌍 What is your timezone? [$DEFAULT_TIMEZONE]: " TIMEZONE
+TIMEZONE=${TIMEZONE:-$DEFAULT_TIMEZONE}
 ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 hwclock --systohc
 echo "TIMEZONE=\"$TIMEZONE\"" >> config.sh
 
 # --- Locale ---
-read -rp "🗣️ What locale do you want to use? [en_US]: " LOCALE
-LOCALE=${LOCALE:-en_US}
+DEFAULT_LOCALE="en_US"
+read -rp "🗣️ What locale do you want to use? [$DEFAULT_LOCALE]: " LOCALE
+LOCALE=${LOCALE:-$DEFAULT_LOCALE}
 sed -i "s/^#${LOCALE} UTF-8/${LOCALE} UTF-8/" /etc/locale.gen
 locale-gen
 echo "LANG=${LOCALE}.UTF-8" > /etc/locale.conf
 echo "LOCALE=\"$LOCALE\"" >> config.sh
 
 # --- Keyboard Layout ---
-read -rp "⌨️ What keyboard layout do you want? [us]: " KEYMAP
-KEYMAP=${KEYMAP:-us}
+DEFAULT_KEYMAP="us"
+read -rp "⌨️ What keyboard layout do you want? [$DEFAULT_KEYMAP]: " KEYMAP
+KEYMAP=${KEYMAP:-$DEFAULT_KEYMAP}
 echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
 echo "KEYMAP=\"$KEYMAP\"" >> config.sh
 
