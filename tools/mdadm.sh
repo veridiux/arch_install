07f246673e4f04916e61da7e8fe7610ef62e5199
Veridiux <veridiux@gmail.com>
2025-07-30 22:29:09 -0500

    mdadm added

--- tools/mdadm.sh
+++ tools/mdadm.sh
@@ -8,6 +8,10 @@ pacman -Sy --noconfirm mdadm
 echo "==> Generating mdadm.conf"
 mdadm --detail --scan > /etc/mdadm.conf
 
+echo "==> Adding mdadm_udev to mkinitcpio.conf"
+# This preserves everything before and after "block"
+sed -i 's/\(HOOKS=.*\)block/\1mdadm_udev block/' /etc/mkinitcpio.conf
+
 echo "==> Rebuilding initramfs"
 mkinitcpio -P
 
