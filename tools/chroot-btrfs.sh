4963e8dd2e946231c3ea8d01ee6e0b503e7adaf8
veridiux <veridiux@gmail.com>
2025-08-12 19:34:47 -0500

    Update chroot-btrfs.sh
    updated efi chroot

--- tools/chroot-btrfs.sh
+++ tools/chroot-btrfs.sh
@@ -14,15 +14,15 @@ mkdir -p /mnt/{home,cache,log,snapshots}
 mount -o subvol=@home "$ROOT_DEV" /mnt/home
 mount -o subvol=@cache "$ROOT_DEV" /mnt/cache
 mount -o subvol=@log "$ROOT_DEV" /mnt/log
-mount -o subvol=@snapshots "$ROOT_DEV" /mnt/snapshots
+#mount -o subvol=@snapshots "$ROOT_DEV" /mnt/snapshots
 
 # Mount boot partition (adjust path if it's /boot/efi)
-mkdir -p /mnt/boot
-mount "$BOOT_DEV" /mnt/boot
+#mkdir -p /mnt/boot
+#mount "$BOOT_DEV" /mnt/boot
 
 # If using EFI and boot is under /boot/efi, uncomment:
-# mkdir -p /mnt/boot/efi
-# mount "$BOOT_DEV" /mnt/boot/efi
+ mkdir -p /mnt/efi
+ mount "$BOOT_DEV" /mnt/efi
 
 # Bind system directories
 for dir in proc sys dev run; do
